7c6e0c91e3f417c9591fb8c8ac830e477a2009d4
yturf <150423924+yturf@users.noreply.github.com>
2023-11-10 22:05:42 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yturf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            float number = 3.5f;
+            double pen = 15;
+            bool haveMoney = true;
+            long cloud = 2000;
+            char letter = 'A';
+            byte cud = 10;
+            string titleCity = "Paris";
+            short greenCars = 2;
+            int burgers = 5;
+            uint bigHouse = 1;
+
+            
+            
+
+        }
+    }
+}
