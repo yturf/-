d519fa0a782eb7fda5c20287ff898c1e39a7876d
yturf <150423924+yturf@users.noreply.github.com>
2023-11-14 08:17:58 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Перестановка_местами1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "ivanov";
+            string surname = "petr";
+
+            Console.WriteLine(name);
+            Console.WriteLine(surname);          
+
+            Console.WriteLine(surname);
+            Console.WriteLine(name);
+
+            Console.ReadKey();
+        }
+    }
+}
