a73ab57c3ddc93ab6a589859e812b279e9a544a7
yturf <150423924+yturf@users.noreply.github.com>
2023-11-15 13:18:01 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,26 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Магазин_кристаллов
+namespace ConsoleApp6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int money;
-            int crystals;
-            int crystalUnitPrice = 15;
+            int numberOfPeopleInQueue;
+            int fixedReceptionTime = 10;
+            int hour;
+            int minute;
 
-            Console.Write("Сколько у вас золота? ");
-            money = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество старушек в очереди: ");
+            numberOfPeopleInQueue = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write($"Здраствуйте! Один кристалл у нас стоит {crystalUnitPrice} золотых монет. Сколько вы хотите купить? ");
-            crystals = Convert.ToInt32(Console.ReadLine());
+            hour = numberOfPeopleInQueue * fixedReceptionTime / 60;
+            minute = numberOfPeopleInQueue * fixedReceptionTime % 60;
 
-            money -= crystals * crystalUnitPrice;
-            Console.Write($"Вы купили {crystals} кристаллов и у вас осталось {money} монет.");
-
+            Console.WriteLine("Вам нужно отстоять в очереди " + hour + " часа и " + minute + " минут.");
+                     
             Console.ReadKey();
+
         }
     }
 }
