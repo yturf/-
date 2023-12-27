488deec53c9554c5cefb7b07f4e638785a91e555
yturf <150423924+yturf@users.noreply.github.com>
2023-12-27 21:21:14 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,107 +1,104 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Конвертор_валют
+namespace Конвертор_валют_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string saleCurrency; 
-            string purchaseCurrency;           
+            double balanceInRub;
+            double balanceInUsd;
+            double balanceInEur;
 
-            double balanceInRUB;
-            double balanceInUSD;
-            double balanceInEUR;
+            double rateRubToUsd = 0.01;
+            double rateRubToEur = 0.01;
 
-            double rateRUBtoUSD = 0.01;
-            double rateRUBtoEUR = 0.01;
+            double rateUsdToRub = 88.61;
+            double rateUsdToEur = 0.91;
 
-            double rateUSDtoRUB = 88.61;
-            double rateUSDtoEUR = 0.91;
+            double rateEurToRub = 97.06;
+            double rateEurToUsd = 1.09;
 
-            double rateEURtoRUB = 97.06;
-            double rateEURtoUSD = 1.09;
-
-            double compareSum = 0;
-            double amountOfCurrencySale = 0; 
+            string typeOfOperation = "";
+            double amountOfCurrencySale = 0;
+            string wordToExit = "";
 
             Console.WriteLine("Добро пожаловать в наш конвертор валют!");
-            Console.Write("Введите количество рублей на вашем счёте: ");
-            balanceInRUB = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Введите количество долларов на вашем счёте: ");
-            balanceInUSD = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Введите количество евро на вашем счёте: ");
-            balanceInEUR = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Какую валюту вы хотите продать?\n 1 - рубли, 2 - доллары, 3 - евро.\n");
-            saleCurrency = Console.ReadLine();
-
-            if (saleCurrency == "1")
-                compareSum = balanceInRUB;
-            else if (saleCurrency == "2")
-                compareSum = balanceInUSD;
-            else if (saleCurrency == "3")
-                compareSum = balanceInEUR;
-
-            Console.Write("На какую сумму вы хотите продать?\n");
-            amountOfCurrencySale = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("В какую валюту вы хотите конвертировать средства?\n 1 - в рубли, 2 - в доллары, 3 - в евро.\n");
-            purchaseCurrency = Console.ReadLine();
-
-            if (purchaseCurrency == "1")
-            {
-                if (saleCurrency == "2")
-                {
-                    balanceInRUB += Convert.ToDouble(amountOfCurrencySale) * rateUSDtoRUB;
-                    balanceInUSD -= amountOfCurrencySale;
-                }
-                else if (saleCurrency == "3")
-                {
-                    balanceInRUB += Convert.ToDouble(amountOfCurrencySale) * rateEURtoRUB;
-                    balanceInEUR -= amountOfCurrencySale;
-                }
-            }
-
-            if (purchaseCurrency == "2")
-            {
-                if (saleCurrency == "1")
-                {
-                    balanceInUSD += Convert.ToDouble(amountOfCurrencySale) * rateRUBtoUSD;
-                    balanceInRUB -= amountOfCurrencySale;
-                }
-                else if (saleCurrency == "3")
-                {
-                    balanceInUSD += Convert.ToDouble(amountOfCurrencySale) * rateEURtoUSD;
-                    balanceInEUR -= amountOfCurrencySale;
-                }
-            }
-
-            if (purchaseCurrency == "3")
-            {
-                if (saleCurrency == "1")
-                {
-                    balanceInEUR += Convert.ToDouble(amountOfCurrencySale) * rateRUBtoEUR;
-                    balanceInRUB -= amountOfCurrencySale;
-                }
-                else if (saleCurrency == "2")
-                {
-                    balanceInEUR += Convert.ToDouble(amountOfCurrencySale) * rateUSDtoEUR;
-                    balanceInUSD -= amountOfCurrencySale;
-                }
-            }
-
-            Console.Write($"Обмен валюты произведен. На вашем счету:\n{balanceInRUB} рублей, " +
-                $"{balanceInUSD} долларов, и {balanceInEUR} евро.\n");
-
             Console.WriteLine("Для выхода из программы введите: Exit");
-            Console.ReadLine();         
+
+                Console.Write("Введите количество рублей на вашем счёте: ");
+             balanceInRub = Convert.ToDouble(Console.ReadLine());
+
+             Console.Write("Введите количество долларов на вашем счёте: ");
+             balanceInUsd = Convert.ToDouble(Console.ReadLine());
+
+             Console.Write("Введите количество евро на вашем счёте: ");
+             balanceInEur = Convert.ToDouble(Console.ReadLine());
+
+            while (wordToExit != "Exit")
+            {               
+             Console.Write("Какую операцию вы хотите провести?\n1 - обменять рубли на доллары, 2 - обменять рубли на евро.\n" +
+                "3 - обменять доллары на рубли, 4 - обменять доллары на евро.\n" +
+                "5 - обменять евро на рубли, 6 - обменять евро на доллары.\n" +
+                "");
+             typeOfOperation = Console.ReadLine();
+
+             Console.Write("На какую сумму вы хотите продать?\n");
+             amountOfCurrencySale = Convert.ToDouble(Console.ReadLine());
+
+             if (typeOfOperation == "1")
+             {
+                balanceInUsd = balanceInRub + Convert.ToDouble(amountOfCurrencySale) * rateRubToUsd;
+                balanceInRub -= amountOfCurrencySale;
+             }
+
+             if (typeOfOperation == "2")
+             {
+                balanceInEur = balanceInRub + Convert.ToDouble(amountOfCurrencySale) * rateRubToEur;
+                balanceInRub -= amountOfCurrencySale;
+             }
+
+             if (typeOfOperation == "3")
+             {
+                balanceInRub = balanceInUsd + Convert.ToDouble(amountOfCurrencySale) * rateUsdToRub;
+                balanceInUsd -= amountOfCurrencySale;
+             }
+
+             if (typeOfOperation == "4")
+             {
+                balanceInEur = balanceInUsd + Convert.ToDouble(amountOfCurrencySale) * rateUsdToEur;
+                balanceInUsd -= amountOfCurrencySale;
+             }
+
+             if (typeOfOperation == "5")
+             {
+                balanceInRub = balanceInEur + Convert.ToDouble(amountOfCurrencySale) * rateEurToRub;
+                balanceInEur -= amountOfCurrencySale;
+             }
+
+             if (typeOfOperation == "6")
+             {
+                balanceInUsd = balanceInEur + Convert.ToDouble(amountOfCurrencySale) * rateEurToUsd;
+                balanceInEur -= amountOfCurrencySale;
+                 if (balanceInRub < 0 || balanceInUsd < 0 || balanceInEur < 0) 
+                 {               
+                    Console.WriteLine("У вас недостаточно средств");                    
+                 }            
+             }
+           
+             Console.Write($"Обмен валюты произведен. На вашем счету:\n{balanceInRub} рублей, " +
+             $"{balanceInUsd} долларов, и {balanceInEur} евро.\n");
+
+             Console.WriteLine("Для продолжения нажмите Enter,\n" +
+                   "Для выхода из программы введите Exit и нажмите Enter");
+
+                wordToExit = Console.ReadLine();
+            }           
         }
     }
 }
