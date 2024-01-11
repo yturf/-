c02aaeec18ee4c77309fcef14ababf5a3c51a7bb
yturf <150423924+yturf@users.noreply.github.com>
2024-01-11 08:59:17 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,176 +1,89 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Конвертер_валют
+namespace Консольное_меню
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            double balanceInRub;
-            double balanceInUsd;
-            double balanceInEur;
-
-            double rateRubToUsd = 0.01;
-            double rateRubToEur = 0.01;
-
-            double rateUsdToRub = 88.61;
-            double rateUsdToEur = 0.91;
-
-            double rateEurToRub = 97.06;
-            double rateEurToUsd = 1.09;
-
-            double amountOfCurrencySale = 0;
-            bool isObieg = true;
+            string setPassword = "";
+            string commandExit = "Esc";
+            string setNameApplication = "";
             string userInput = "";
-            string commandExit = "Exit";
 
-            string commandToExchangeRubForUsd = "1";
-            string commandToExchangeRubForEur = "2";
-            string commandToExchangeUsdForRub = "3";
-            string commandToExchangeUsdForEur = "4";
-            string commandToExchangeEurForRub = "5";
-            string commandToExchangeEurForUsd = "6";
+            string colorWhite = "1";
+            string colorGreen = "2";
+            string colorYellow = "3";
 
-            Console.WriteLine("Добро пожаловать в наш конвертор валют!");
+            bool isObieg = true;
 
-            Console.Write("Введите количество рублей на вашем счёте: ");
-            balanceInRub = Convert.ToDouble(Console.ReadLine());
+            int triesCount = 3;
 
-            Console.Write("Введите количество долларов на вашем счёте: ");
-            balanceInUsd = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Установите пароль для вашего приложения: ");
+            setPassword = Console.ReadLine();
 
-            Console.Write("Введите количество евро на вашем счёте: ");
-            balanceInEur = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
 
-            Console.WriteLine("Для выхода из программы введите: " + commandExit + " и нажмите Enter");
+            Console.Write("Будьте внимательны! У вас 3 попытки!\nВведите пароль для входа в приложение: ");
 
-            while (isObieg)
+            for (int i = 0; i < triesCount; i++)
             {
-                Console.Write("Какую операцию вы хотите провести?\n" +
-                   commandToExchangeRubForUsd + " - обменять рубли на доллары, " + 
-                   commandToExchangeRubForEur + " - обменять рубли на евро.\n" +
-                   commandToExchangeUsdForRub + " - обменять доллары на рубли, " +
-                   commandToExchangeUsdForEur + " - обменять доллары на евро.\n" +
-                   commandToExchangeEurForRub + " - обменять евро на рубли, " +
-                   commandToExchangeEurForUsd + " - обменять евро на доллары.\n" +
-                   "");
                 userInput = Console.ReadLine();
 
-                if (userInput == commandToExchangeRubForUsd)
+                if (userInput == setPassword) 
                 {
-                        Console.Write("На какую сумму вы хотите продать?\n");
-                        amountOfCurrencySale = Convert.ToDouble(Console.ReadLine());
-
-                    if (balanceInRub >= amountOfCurrencySale)
-                    {
-                        balanceInUsd += amountOfCurrencySale * rateRubToUsd;
-                        balanceInRub -= amountOfCurrencySale;
-                        Console.Write($"Обмен валюты произведен. На вашем счету:\n{balanceInRub} рублей, " +
-                        $"{balanceInUsd} долларов, и {balanceInEur} евро.\n");
-                    }
-                    else 
-                    {
-                        Console.WriteLine("У вас недостаточно средств");
-                    }
+                    Console.WriteLine("Пароль принят! Доступ открыт!");
+                    break;
                 }
-                else if (userInput == commandToExchangeRubForEur)
+                else  
                 {
-                        Console.Write("На какую сумму вы хотите продать?\n");
-                        amountOfCurrencySale = Convert.ToDouble(Console.ReadLine());
-
-                    if (balanceInRub >= amountOfCurrencySale)
-                    {
-                        balanceInEur += amountOfCurrencySale * rateRubToEur;
-                        balanceInRub -= amountOfCurrencySale;
-                        Console.Write($"Обмен валюты произведен. На вашем счету:\n{balanceInRub} рублей, " +
-                        $"{balanceInUsd} долларов, и {balanceInEur} евро.\n");
-                    }
-                    else 
-                    {
-                        Console.WriteLine("У вас недостаточно средств");
-                    }
+                    Console.WriteLine("Пароль неверный! Доступ закрыт! ");
+                    Console.WriteLine("У вас осталось попыток: " + (triesCount - (i + 1)));
                 }
-                else if (userInput == commandToExchangeUsdForRub)
-                {
-                        Console.Write("На какую сумму вы хотите продать?\n");
-                        amountOfCurrencySale = Convert.ToDouble(Console.ReadLine());
-
-                    if (balanceInUsd >= amountOfCurrencySale)
-                    {
-                        balanceInRub += amountOfCurrencySale * rateUsdToRub;
-                        balanceInUsd -= amountOfCurrencySale;
-                        Console.Write($"Обмен валюты произведен. На вашем счету:\n{balanceInRub} рублей, " +
-                        $"{balanceInUsd} долларов, и {balanceInEur} евро.\n");
-                    }
-                    else
-                    {
-                        Console.WriteLine("У вас недостаточно средств");
-                    }
-                }
-                else if (userInput == commandToExchangeUsdForEur)
-                {
-                        Console.Write("На какую сумму вы хотите продать?\n");
-                        amountOfCurrencySale = Convert.ToDouble(Console.ReadLine());
-
-                    if (balanceInUsd >= amountOfCurrencySale)
-                    {
-                        balanceInEur += amountOfCurrencySale * rateUsdToEur;
-                        balanceInUsd -= amountOfCurrencySale;
-                        Console.Write($"Обмен валюты произведен. На вашем счету:\n{balanceInRub} рублей, " +
-                        $"{balanceInUsd} долларов, и {balanceInEur} евро.\n");
-                    }
-                    else
-                    {
-                        Console.WriteLine("У вас недостаточно средств");
-                    }
-                }
-                else if (userInput == commandToExchangeEurForRub)
-                {
-                        Console.Write("На какую сумму вы хотите продать?\n");
-                        amountOfCurrencySale = Convert.ToDouble(Console.ReadLine());
-
-                    if (balanceInEur >= amountOfCurrencySale)
-                    {
-                        balanceInRub += amountOfCurrencySale * rateEurToRub;
-                        balanceInEur -= amountOfCurrencySale;
-                        Console.Write($"Обмен валюты произведен. На вашем счету:\n{balanceInRub} рублей, " +
-                        $"{balanceInUsd} долларов, и {balanceInEur} евро.\n");
-                    }
-                    else
-                    {
-                        Console.WriteLine("У вас недостаточно средств");
-                    }
-                }
-                else if (userInput == commandToExchangeEurForUsd)
-                {
-                        Console.Write("На какую сумму вы хотите продать?\n");
-                        amountOfCurrencySale = Convert.ToDouble(Console.ReadLine());
-
-                    if (balanceInEur >= amountOfCurrencySale)
-                    {
-                        balanceInUsd += amountOfCurrencySale * rateEurToUsd;
-                        balanceInEur -= amountOfCurrencySale;
-                        Console.Write($"Обмен валюты произведен. На вашем счету:\n{balanceInRub} рублей, " +
-                        $"{balanceInUsd} долларов, и {balanceInEur} евро.\n");
-                    }
-                    else
-                    {
-                        Console.WriteLine("У вас недостаточно средств");
-                    }
-                }  
-                else if (userInput == commandExit)
-                {
-                    isObieg = false;
-                }
-
-                Console.WriteLine("Для продолжения нажмите Enter,\n" +
-                      "Для выхода из программы введите " + commandExit + " и нажмите Enter");
             }
+
+            if (userInput == setPassword)
+            {
+                    Console.WriteLine("Для выхода из программы введите: " + commandExit);
+                    Console.WriteLine("Дайте название для приложения: ");
+                    setNameApplication = Console.ReadLine();
+
+                    Console.Clear();
+
+                while (isObieg)
+                {
+                    Console.WriteLine("Задайте цвет для своей консоли:\n" +
+                        colorWhite +  " - белый\n" +
+                        colorGreen + " - зелёный\n" +
+                        colorYellow + " - желтый");
+
+                    Console.WriteLine("Добро пожаловать в приложение - " + setNameApplication);
+
+                    userInput = Console.ReadLine();
+
+                    if (userInput == commandExit || setNameApplication == commandExit)
+                    {
+                        isObieg = false;
+
+                    }
+                     if (userInput == colorWhite)
+                     {
+                         Console.BackgroundColor = ConsoleColor.White;
+                     }
+                     else if (userInput == colorGreen)
+                     {
+                         Console.BackgroundColor = ConsoleColor.Green;
+                     }
+                     else if (userInput == colorYellow)
+                     {
+                         Console.BackgroundColor = ConsoleColor.Yellow;
+                     }
+                }
+            }                             
         }
     }
 }
