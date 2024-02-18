f534ebd3cbe578e27242f4362f4212de7fbc5115
yturf <150423924+yturf@users.noreply.github.com>
2024-02-18 23:10:58 +0500

    Update MoveCapsule.cs

--- MoveCapsule.cs
+++ MoveCapsule.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveCapsule : MonoBehaviour
+public class CapsuleMove : MonoBehaviour
 {
     [SerializeField] private float _scaleSpeed;
   
-    void Update()
+    private void Update()
     {
         transform.localScale *= _scaleSpeed;
     }
