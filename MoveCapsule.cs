c733b448f18230872b63008dcafb26e99bf984f3
yturf <150423924+yturf@users.noreply.github.com>
2024-02-19 20:24:03 +0500

    Update CapsuleScaler.cs

--- MoveCapsule.cs
+++ MoveCapsule.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class CapsuleScaler : MonoBehaviour
 {
     [SerializeField] private float _scaleSpeed;
+
+    Vector3 newScale = new Vector3(0.1f, 0.1f, 0.1f);
   
     private void Update()
     {
-        transform.localScale *= _scaleSpeed;
+        transform.localScale += newScale * _scaleSpeed * Time.deltaTime;
     }
 }
