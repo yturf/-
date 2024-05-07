7c36d7ae1ecb160748ddfea05ae02e5a2d1929e8
yturf <150423924+yturf@users.noreply.github.com>
2024-05-07 16:20:51 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +12,6 @@ namespace Динамический_массив
         {
             int arraySize;
             int sumArrayInitial = 0;
-            int numberForArray = 0;
 
             string userInput = "";
             string getSumArray = "sum";
@@ -33,19 +31,20 @@ namespace Динамический_массив
                 arrayInitial[i] = Convert.ToInt32(Console.ReadLine());
             }
 
-            Console.Clear();
-
-            Console.WriteLine("Для получения суммы всех чисел в массиве введите команду: sum.");
-            Console.WriteLine("Для выхода из программы введите команду: exit.");
-            Console.WriteLine("Вывод исходного массива:");
-
             while (isWork)
             {
+                Console.Clear();
+
+                Console.WriteLine("Чтобы узнать сумму всех введенных чисел массива, " +
+                    "введите команду - sum");
+                Console.WriteLine("Чтобы выйти из программы введите команду - exit");
+                Console.WriteLine("\nИсходный массив:");
+
                 for (int i = 0; i < arrayInitial.Length; i++)
                 {
-                    Console.Write(arrayInitial[i] + " ");
-                    sumArrayInitial += arrayInitial[i];
                     augmentedArray[i] = arrayInitial[i];
+                    sumArrayInitial += arrayInitial[i];
+                    Console.Write(arrayInitial[i] + " ");
                 }
 
                 Console.WriteLine("\nВведите команду: ");
@@ -53,30 +52,31 @@ namespace Динамический_массив
 
                 if (userInput == getSumArray)
                 {
-                    Console.WriteLine("\nСумма всех введенных чисел массива: " + sumArrayInitial);
+                    Console.WriteLine("Сумма всех введенных чисел исходного массива равна: " + sumArrayInitial);
                 }
 
-                if (userInput == commandToExit)
+                else if (userInput == commandToExit)
                 {
                     isWork = false;
                 }
 
                 else
                 {
+                    Console.WriteLine("\nНовый массив:");
+
                     arrayInitial = augmentedArray;
                     augmentedArray[augmentedArray.Length - 1] = Convert.ToInt32(userInput);
 
-                    for (int i = 0; i < arrayInitial.Length; i++)
+                    for (int i = 0; i < augmentedArray.Length; i++)
                     {
                         arrayInitial[i] = augmentedArray[i];
-                        Console.Write(arrayInitial[i] + " ");
+                        Console.Write(augmentedArray[i] + " ");
                     }
                 }
 
-                Console.Clear();
+                Console.ReadKey();
             }
 
-            Console.ReadKey();
         }
     }
 }
