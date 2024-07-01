f0e38d9ca4b75b1fd891947a0a66b39f69e842ce
yturf <150423924+yturf@users.noreply.github.com>
2024-07-01 19:15:57 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -12,77 +12,91 @@ namespace Динамический_массив
             int arraySize;
             int sumArrayInitial = 0;
 
-            string userInput = "";
+            string userInput;
 
             bool isWork = true;
 
             Console.WriteLine("Чтобы узнать сумму всех введенных чисел массива, " +
-             "введите команду - sum");
-            Console.WriteLine("Чтобы выйти из программы введите команду - exit");
+             "введите команду - " + CommandGetSumArray);
+            Console.WriteLine("Чтобы выйти из программы введите команду - " + CommandToExit);
 
             while (isWork)
-            {            
-                Console.Write("\nВведите размер массива: ");
-                arraySize = Convert.ToInt32(Console.ReadLine());
-
-                int[] arrayInitial = new int[arraySize];
-
-                for (int i = 0; i < arrayInitial.Length; i++)
+            {
+                try
                 {
-                    Console.WriteLine($"Введите элемент массива под индексом {i}");
-                    arrayInitial[i] = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("\nВведите размер массива: ");
+                    arraySize = Convert.ToInt32(Console.ReadLine());
+
+                    int[] arrayInitial = new int[arraySize];
+
+                    for (int i = 0; i < arrayInitial.Length; i++)
+                    {
+                        Console.WriteLine($"Введите элемент массива под индексом {i}");
+                        arrayInitial[i] = Convert.ToInt32(Console.ReadLine());
+                    }
+
+                    Console.WriteLine("\nИсходный массив:");
+
+                    for (int i = 0; i < arrayInitial.Length; i++)
+                    {
+                        Console.Write(arrayInitial[i] + " ");
+                    }
+
+                    Console.WriteLine("\nВведите команду: ");
+                    userInput = Console.ReadLine();
+
+                    switch (userInput)
+                    {
+                        case CommandGetSumArray:
+                            {
+                                for (int i = 0; i < arrayInitial.Length; i++)
+                                {
+                                    sumArrayInitial += arrayInitial[i];
+                                }
+                                Console.WriteLine("Сумма всех введенных чисел исходного массива равна: " + sumArrayInitial);
+                            }
+                            break;
+
+                        case CommandToExit:
+                            {
+                                isWork = false;
+                            }
+                            break;
+
+                        default:
+                            {
+                                int[] augmentedArray = new int[arrayInitial.Length + 1];
+
+                                for (int i = 0; i < arrayInitial.Length; i++)
+                                {
+                                    augmentedArray[i] = arrayInitial[i];
+                                }
+                                augmentedArray[augmentedArray.Length - 1] = Convert.ToInt32(userInput);
+                                arrayInitial = augmentedArray;
+
+                                Console.Write("\nНовый массив: ");
+
+                                for (int i = 0; i < arrayInitial.Length; i++)
+                                {
+                                    Console.Write(arrayInitial[i] + " ");
+                                }
+
+                            }
+                            break;
+                    }
                 }
 
-                Console.WriteLine("\nИсходный массив:");
-
-                for (int i = 0; i < arrayInitial.Length; i++)
+                catch (FormatException)
                 {
-                    Console.Write(arrayInitial[i] + " ");
+                    Console.WriteLine("Вы ввели неверный формат!");
                 }
-
-                Console.WriteLine("\nВведите команду: ");
-                userInput = Console.ReadLine();
-
-                switch (userInput)
+                catch (Exception)
                 {
-                    case CommandGetSumArray:
-                        {
-                            for (int i = 0; i < arrayInitial.Length; i++)
-                            {
-                                sumArrayInitial += arrayInitial[i];
-                                Console.Write(arrayInitial[i] + " ");
-                            }
-                            Console.WriteLine("Сумма всех введенных чисел исходного массива равна: " + sumArrayInitial);
-                        }
-                        break;
-
-                    case CommandToExit:
-                        {
-                            isWork = false;
-                        }
-                        break;
-
-                    default:
-                        {
-                            int[] augmentedArray = new int[arrayInitial.Length + 1];
-
-                            for (int i = 0; i < arrayInitial.Length; i++)
-                            {
-                                augmentedArray[i] = arrayInitial[i];
-                            }
-                            augmentedArray[augmentedArray.Length - 1] = Convert.ToInt32(userInput);
-                            arrayInitial = augmentedArray;
-
-                            Console.Write("\nНовый массив: ");
-
-                            for (int i = 0; i < arrayInitial.Length; i++)
-                            {
-                                Console.Write(arrayInitial[i] + " ");
-                            }
-
-                        }
-                        break;
-
+                    Console.WriteLine("Какое-то исключение!");
+                }
+                finally
+                {
+                    Console.WriteLine("\nРабота завершена!");
                 }
             }
         }
