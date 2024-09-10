9cb1b53e3a65ba7839393dd19e74f9545dc3ad86
yturf <150423924+yturf@users.noreply.github.com>
2024-09-10 13:31:38 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -19,53 +19,48 @@ namespace Динамический_массив
 
             int[] arrayInitial = new int[] { };
 
-            for (int i = 0; i < arrayInitial.Length; i++)
-            {
-                
-            }
-
-            for (int i = 0; i < arrayInitial.Length; i++)
-            {
-                Console.Write(arrayInitial[i] + " ");
-            }
-
             while (isWork)
             {
+                for (int i = 0; i < arrayInitial.Length; i++)
+                {
+                    Console.Write("\nСтарый массив: " + arrayInitial[i]);
+                }
+
                 Console.WriteLine("\nВведите команду: ");
                 userInput = Console.ReadLine();
 
                 switch (userInput)
                 {
                     case CommandGetSumArray:
-                            int sum = 0;
+                        int sum = 0;
 
-                            for (int i = 0; i < arrayInitial.Length; i++)
-                            {
-                                sum += arrayInitial[i];
-                            }
-                            Console.WriteLine("Сумма всех введенных чисел исходного массива равна: " + sum);
+                        for (int i = 0; i < arrayInitial.Length; i++)
+                        {
+                            sum += arrayInitial[i];
+                        }
+                        Console.WriteLine("Сумма всех введенных чисел исходного массива равна: " + sum);
                         break;
 
                     case CommandToExit:
-                            isWork = false;
-                        break; 
+                        isWork = false;
+                        break;
 
                     default:
-                            int[] temporaryArray = new int[arrayInitial.Length + 1];
+                        int[] temporaryArray = new int[arrayInitial.Length + 1];
 
-                            for (int i = 0; i < arrayInitial.Length; i++)
-                            {
-                                temporaryArray[i] = arrayInitial[i];
-                            }
-                            temporaryArray[temporaryArray.Length - 1] = Convert.ToInt32(userInput);
-                            arrayInitial = temporaryArray;
+                        for (int i = 0; i < arrayInitial.Length; i++)
+                        {
+                            temporaryArray[i] = arrayInitial[i];
+                        }
+                        temporaryArray[temporaryArray.Length - 1] = Convert.ToInt32(userInput);
+                        arrayInitial = temporaryArray;
 
-                            Console.Write("\nНовый массив: ");
+                        Console.Write("\nНовый массив: ");
 
-                            for (int i = 0; i < arrayInitial.Length; i++)
-                            {
-                                Console.Write(arrayInitial[i] + " ");
-                            }
+                        for (int i = 0; i < arrayInitial.Length; i++)
+                        {
+                            Console.Write(arrayInitial[i] + " ");
+                        }
                         break;
 
                 }
