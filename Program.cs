5e5eb86deefd257dcbad8886c06fecd5b90a8b30
yturf <150423924+yturf@users.noreply.github.com>
2024-10-02 10:14:20 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,23 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Split
+namespace Сдвиг_значений_массива
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string sentence = "Split используется для разрыва строки с разделителями на подстроки.";
+            const int lastNumber = 9;
 
-            char[] separators = new char[] { ' ', '.' };
+            int[] array = new int[5] { 4, 6, 8, 33, 9 };
 
-            string[] arrayOfWords = sentence.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            int userInput;
 
-            foreach (string word in arrayOfWords)
+            Console.Write("Массив: ");
+
+            for (int i = 0; i < array.Length; i++)
             {
-                Console.WriteLine($"Слово: {word}");
+                Console.Write(array[i] + " ");
             }
 
+            Console.Write("\nУкажите значение для сдвига массива влево: ");
+            userInput = Convert.ToInt32(Console.Read());
+
+            for (int i = 0; i < userInput; i++)
+            {
+                int lastArray = array[array.Length - 1];
+                for (int j = array.Length - 1; j > 0; j--)
+                    array[j] = array[j - 1];
+                array[0] = lastArray;
+            }
+
+            Console.WriteLine("Сдвинутый массив: ");
+
+            for (int i = 0; i < array.Length; ++i)
+
+            Console.Write(array[i] + " ");
             Console.ReadKey();
         }
     }
