97d5ddf6b059f219f72b73b36ab38c2aba4b62ac
yturf <150423924+yturf@users.noreply.github.com>
2024-10-03 12:40:45 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,11 +10,9 @@ namespace Сдвиг_значений_массива
     {
         static void Main(string[] args)
         {
-            const int lastNumber = 9;
-
             int[] array = new int[5] { 4, 6, 8, 33, 9 };
 
-            int userInput;
+            int positionShift;
 
             Console.Write("Массив: ");
 
@@ -24,21 +22,27 @@ namespace Сдвиг_значений_массива
             }
 
             Console.Write("\nУкажите значение для сдвига массива влево: ");
-            userInput = Convert.ToInt32(Console.Read());
+            positionShift = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 0; i < userInput; i++)
+            int newPositionShift = positionShift % array.Length;
+
+            for (int i = 0; i < newPositionShift; i++)
             {
                 int lastArray = array[array.Length - 1];
+
                 for (int j = array.Length - 1; j > 0; j--)
                     array[j] = array[j - 1];
+
                 array[0] = lastArray;
             }
 
-            Console.WriteLine("Сдвинутый массив: ");
+            Console.Write("Сдвинутый массив: ");
 
             for (int i = 0; i < array.Length; ++i)
+            {
+                Console.Write(array[i] + " ");
+            }
 
-            Console.Write(array[i] + " ");
             Console.ReadKey();
         }
     }
