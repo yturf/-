2cfcd440ecbcaac940e77116d014c4c6738263fa
yturf <150423924+yturf@users.noreply.github.com>
2024-11-22 13:35:03 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,48 +1,167 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 
-namespace Канзас_сити_шафл
+namespace Кадровый_учёт
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[] array = new int[6] { 4, 6, 9, 11, 2, 7 };
+            const string CommandAddDossier = "1";
+            const string CommandPrintAllTheDossier = "2";
+            const string CommandDeleteTheDossier = "3";
+            const string CommandSearchByLastName = "4";
+            const string CommandExit = "5";
 
-            Show(array, "Исходный массив");
+            string[] listInitials = { "Иванов Пётр Сергеевич", "Козлов Виталий Александрович" };
+            string[] listPositions = { "Маляр", "Плотник" };
 
-            Shuffle(array);
+            bool isOpen = true;
 
-            Show(array, "\nПеремешанный массив");
+            while (isOpen)
+            {
+                Console.SetCursorPosition(5, 0);
+                Console.WriteLine("Меню");
+                Console.WriteLine("Добавить досье(нажмите) - " + CommandAddDossier);
+                Console.WriteLine("Вывести всё досье(нажмите) - " + CommandPrintAllTheDossier);
+                Console.WriteLine("Удалить досье(нажмите) - " + CommandDeleteTheDossier);
+                Console.WriteLine("Поиск по фамилии(нажмите) - " + CommandSearchByLastName);
+                Console.WriteLine("Выход(нажмите) - " + CommandExit);
 
-            Console.ReadKey();
+                string userInput = Console.ReadLine();
+                int index = 0;
+
+                switch (userInput)
+                {
+                    case CommandAddDossier:
+                        AddInListOfDossiers(ref listInitials, ref listPositions);
+                        break;
+                    case CommandPrintAllTheDossier:
+                        ShowDossier(listInitials, listPositions);
+                        break;
+                    case CommandDeleteTheDossier:
+                        RemoveAt(ref listInitials, ref listPositions, ref index);
+                        break;
+                    case CommandSearchByLastName:
+                        SearchByLastName(listInitials, listPositions);
+                        break;
+                    case CommandExit:
+                        isOpen = false;
+                        break;
+                    default:
+                        Console.WriteLine("Ввод не распознан введите еще раз");
+                        break;
+                }
+
+                Console.WriteLine("Для продолжения нажмите любую кнопку");
+                Console.ReadKey();
+                Console.Clear();
+            }
         }
 
-        static void Shuffle(int[] array)
+        static void AddInListOfDossiers(ref string[] listInitials, ref string[] listPositions)
         {
-            Random random = new Random();
+            Console.WriteLine($"Введите ФИО сотрудника: ");
+            listInitials = AddDossier(Console.ReadLine(), listInitials);
 
-            for (int i = 0; i < array.Length; i++)
+            Console.WriteLine($"Введите должность сотрудника: ");
+            listPositions = AddDossier(Console.ReadLine(), listPositions);
+
+            ShowDossier(listInitials, listPositions);
+
+            Console.Clear();
+        }
+
+        static string[] AddDossier(string text, string[] oldArray)
+        {
+            oldArray = ExpandArray(oldArray);
+            oldArray[oldArray.Length - 1] = text;
+
+            return oldArray;
+        }
+
+        static string[] ExpandArray(string[] oldArray)
+        {
+            string[] tempArray = new string[oldArray.Length + 1];
+
+            for (int i = 0; i < oldArray.Length; i++)
             {
-                int randomIndexElement = random.Next(array[0], array.Length);
-                int currentArrayElement = array[i];
-
-                array[i] = array[randomIndexElement];
-                array[randomIndexElement] = currentArrayElement;
+                tempArray[i] = oldArray[i];
             }
-        }      
 
-        static void Show(int[] array, string message)
+            return tempArray;
+        }
+
+        static void ShowDossier(string[] tempListInitials, string[] tempListPositions)
         {
-            Console.Write(message + ": ");
+            Console.WriteLine("Список досье: ");
 
-            for (int i = 0; i < array.Length; i++)
+            for (int i = 0; i < tempListInitials.Length; i++)
             {
-                Console.Write(array[i] + " ");
+                Console.WriteLine($"Сотрудник под номером {i + 1}: {tempListInitials[i]}: {tempListPositions[i]}");
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        static void RemoveAt(ref string[] firstArray, ref string[] secondArray, ref int index)
+        {
+            CheckingTheInputForDeletion(ref firstArray, ref secondArray);
+
+            string[] newFirstArray = new string[firstArray.Length - 1];
+            string[] newSecondArray = new string[secondArray.Length - 1];
+
+            for (int i = 0; i < index; i++)
+            {
+                newFirstArray[i] = firstArray[i];
+                newSecondArray[i] = secondArray[i];
+            }
+
+            for (int i = index + 1; i < firstArray.Length && i < secondArray.Length; i++)
+            {
+                newFirstArray[i - 1] = firstArray[i];
+                newSecondArray[i - 1] = secondArray[i];
+            }
+
+            firstArray = newFirstArray;
+            secondArray = newSecondArray;
+
+            Console.Clear();
+        }
+
+        static void CheckingTheInputForDeletion(ref string[] listInitials, ref string[] listPositions)
+        {
+            Console.Write("Введите номер досье которое хотите удалить: ");
+
+            while (int.TryParse(Console.ReadLine(), out int indexToDelete) == false || indexToDelete > listInitials.Length || indexToDelete <= 0)
+            {
+                Console.Write("Введеное число не верного формата или под номером нет такого сотрудника, еще раз: ");
+            }
+        }
+
+        static void SearchByLastName(string[] listInitials, string[] listPositions) 
+        {
+            Console.Write("Введите фамилия для поиска: ");
+            string surnameToFind = Console.ReadLine();
+
+            char space = ' ';
+            string[] tempArray;
+            bool doesDossierExist = false;
+
+            for (int i = 0; i < listInitials.Length; i++)
+            {
+                tempArray = listInitials[i].Split(space);
+
+                if (surnameToFind.ToLower() == tempArray[0].ToLower())
+                {
+                    Console.WriteLine($"ФИО сотрудника: {listInitials[i]}. Должность сотрудника: {listPositions[i]}.");
+                    doesDossierExist = true;
+                }
+            }
+
+            if (doesDossierExist == false)
+            {
+                Console.WriteLine("С такой фамилией досье не найдено!");
             }
         }
     }
