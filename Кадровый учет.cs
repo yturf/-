2b127d3fbe517b6f05c4a321f19438af8a4d9efb
yturf <150423924+yturf@users.noreply.github.com>
2024-11-30 00:10:09 +0500

    Add files via upload

--- Кадровый учет.cs
+++ Кадровый учет.cs
@@ -135,7 +135,7 @@ namespace Кадровый_учёт
             Console.Clear();
         }
 
-        static int CheckNumberInput(out int indexToDelete)
+        static void CheckNumberInput(out int indexToDelete)
         {
             Console.Write("Введите номер досье которое хотите удалить: ");
 
@@ -143,19 +143,15 @@ namespace Кадровый_учёт
             {
                 Console.Write("Введеное число не верного формата, еще раз: ");
             }
-
-            return indexToDelete;
         }
 
-        static int CheckNumberRange(string[] fullNames, int indexToDelete)
+        static void CheckNumberRange(string[] fullNames, int indexToDelete)
         {
             while (indexToDelete > fullNames.Length || indexToDelete <= 0)
             {
                 Console.Write("Под таким номером нет сотрудника, еще раз: \n");
                 CheckNumberInput(out indexToDelete);
             }
-
-            return indexToDelete;
         }
 
         static void SearchByLastName(string[] fullNames, string[] fullPositions)
