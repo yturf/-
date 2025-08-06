622cf65b514fbfa3877ec9cf78f3c713b8da2a3c
yturf <150423924+yturf@users.noreply.github.com>
2025-08-06 10:05:06 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,185 +1,157 @@
 ﻿using System;
+using System.IO;
 
-namespace Кадровый_учет
+namespace PacMan
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            const string CommandAddDossier = "1";
-            const string CommandPrintAllDossier = "2";
-            const string CommandDeleteDossier = "3";
-            const string CommandSearchByLastName = "4";
-            const string CommandExit = "5";
+            Console.CursorVisible = false;
 
-            string[] employeeFullNames = { "Иванов Иван Иванович", "Петров Петр Петрович" };
-            string[] employeePositions = { "Инженер", "Плотник" };
+            int pacmanX = 1;
+            int pacmanY = 1;
+            int score = 0;
 
-            bool isOpen = true;
+            bool isOpenGame = true;
 
-            while (isOpen)
+            char[,] map = ReadMap("map.txt");
+
+            ConsoleKeyInfo pressedKey = new ConsoleKeyInfo('w', ConsoleKey.W, false, false, false);
+            
+            Console.WriteLine("Для выхода из игры нажмите клавишу: Esc");
+
+            while (isOpenGame)
             {
-                Console.SetCursorPosition(5, 0);
-                Console.WriteLine("Меню");
-                Console.WriteLine("Добавить досье(нажмите) - " + CommandAddDossier);
-                Console.WriteLine("Вывести всё досье(нажмите) - " + CommandPrintAllDossier);
-                Console.WriteLine("Удалить досье(нажмите) - " + CommandDeleteDossier);
-                Console.WriteLine("Поиск по фамилии(нажмите) - " + CommandSearchByLastName);
-                Console.WriteLine("Выход(нажмите) - " + CommandExit);
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Clear();
+                DrawMap(map);
 
-                string userInput = Console.ReadLine();
+                DrawPlayer(pacmanX, pacmanY);
+                DrawScore(score);
 
-                switch (userInput)
+                pressedKey = Console.ReadKey();
+
+                CloseGame(pressedKey, ref isOpenGame);
+
+                int[] derection = GetDerection(pressedKey);
+                MovePlayer(ref pacmanX, ref pacmanY, map, ref score, derection);
+            }
+        }
+
+        private static char[,] ReadMap(string Path)
+        {
+            string[] file = File.ReadAllLines("map.txt");
+
+            char[,] map = new char[GetMaxLengthOfLine(file), file.Length];
+
+            for (int x = 0; x < map.GetLength(0); x++)
+            {
+                for (int y = 0; y < map.GetLength(1); y++)
                 {
-                    case CommandAddDossier:
-                        AddDossier(ref employeeFullNames, ref employeePositions);
-                        break;
-
-                    case CommandPrintAllDossier:
-                        PrintAllDossier(employeeFullNames, employeePositions);
-                        break;
-
-                    case CommandDeleteDossier:
-                        RemoveDossier(ref employeeFullNames, ref employeePositions);
-                        break;
-
-                    case CommandSearchByLastName:
-                        SearchByLastName(employeeFullNames, employeePositions);
-                        break;
-
-                    case CommandExit:
-                        isOpen = false;
-                        break;
-
-                    default:
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Ввод не распознан введите еще раз");
-                        Console.ResetColor();
-                        break;
+                    map[x, y] = file[y][x];
                 }
+            }
 
-                Console.WriteLine("Для продолжения нажмите любую кнопку");
-                Console.ReadKey();
+            return map;
+        }
+
+        private static void MovePlayer(ref int pacmanX, ref int pacmanY, char[,]
+            map, ref int score, int[] derection)
+        {
+            int nextPacmanPositionX = pacmanX + derection[0];
+            int nextPacmanPositionY = pacmanY + derection[1];
+
+            char spaceChar = ' ';
+            char starChar = '*';
+            char nextCell = map[nextPacmanPositionX, nextPacmanPositionY];
+
+            if (nextCell == spaceChar || nextCell == starChar)
+            {
+                pacmanX = nextPacmanPositionX;
+                pacmanY = nextPacmanPositionY;
+
+                if (nextCell == starChar)
+                {
+                    score++;
+                    map[nextPacmanPositionX, nextPacmanPositionY] = spaceChar;
+                }
+            }
+        }
+
+        private static int[] GetDerection(ConsoleKeyInfo pressedKey)
+        {
+            int[] derection = { 0, 0 };
+
+            ConsoleKey moveUpCommand = ConsoleKey.UpArrow;
+            ConsoleKey moveDownCommand = ConsoleKey.DownArrow;
+            ConsoleKey moveLeftCommand = ConsoleKey.LeftArrow;
+            ConsoleKey moveRightCommand = ConsoleKey.RightArrow;
+
+            if (pressedKey.Key == moveUpCommand)
+                derection[1] = -1;
+            else if (pressedKey.Key == moveDownCommand)
+                derection[1] = 1;
+            else if (pressedKey.Key == moveLeftCommand)
+                derection[0] = -1;
+            else if (pressedKey.Key == moveRightCommand)
+                derection[0] = 1;
+
+            return derection;
+        }
+
+        private static void DrawMap(char[,] map)
+        {
+            for (int y = 0; y < map.GetLength(1); y++)
+            {
+                for (int x = 0; x < map.GetLength(0); x++)
+                {
+                    Console.Write(map[x, y]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void DrawPlayer(int posX, int posY)
+        {
+            string playerChar = "@";
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.SetCursorPosition(posX, posY);
+            Console.WriteLine(playerChar);
+        }
+
+        private static void DrawScore(int score)
+        {
+            Console.SetCursorPosition(35, 0);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"score: {score}");
+        }
+
+        private static int GetMaxLengthOfLine(string[] lines)
+        {
+            int maxLength = lines[0].Length;
+
+            foreach (var line in lines)
+            {
+                if (line.Length > maxLength)
+                {
+                    maxLength = line.Length;
+                }
+            }
+
+            return maxLength;
+        }
+
+        private static bool CloseGame(ConsoleKeyInfo pressedKey, ref bool isOpenGame)
+        {
+            if (pressedKey.Key == ConsoleKey.Escape)
+            {
+                isOpenGame = false;  
                 Console.Clear();
             }
-        }
 
-        private static void AddDossier(ref string[] fullNames, ref string[] positions)
-        {
-            Console.Write("Введите ФИО сотрудника: ");
-            string employeeFullName = Console.ReadLine();
-
-            Console.Write("Введите должность сотрудника: ");
-            string employeePosition = Console.ReadLine();
-
-            fullNames = AddElement(fullNames, employeeFullName);
-            positions = AddElement(positions, employeePosition);
-        }
-
-        private static string[] AddElement(string[] dossier, string element)
-        {
-            string[] tempArray = new string[dossier.Length + 1];
-
-            int length = dossier.Length;
-
-            for (int i = 0; i < length; i++)
-            {
-                tempArray[i] = dossier[i];
-            }
-
-            tempArray[dossier.Length] = element;
-
-            return tempArray;
-        }
-
-        private static void PrintAllDossier(string[] fullNames, string[] positions)
-        {
-            int length = fullNames.Length;
-
-            for (int i = 0; i < length; i++)
-            {
-                Console.WriteLine($"{i + 1}: ФИО сотрудника: {fullNames[i]}. Должность: {positions[i]}.");
-            }
-        }
-
-        private static void RemoveDossier(ref string[] fullNames, ref string[] positions)
-        {
-            if (fullNames.Length == 0)
-            {
-                ChangeColor(ConsoleColor.Red, "Нет досье для удаления!");
-                return;
-            }
-
-            PrintAllDossier(fullNames, positions);
-
-            Console.Write("Введите номер досье которое хотите удалить: ");
-
-            if (int.TryParse(Console.ReadLine(), out int numberToDelete) == false || ValidateIndexRange(numberToDelete - 1, fullNames.Length) == false)
-            {
-                ChangeColor(ConsoleColor.Red, "Некорректный номер досье!");
-                return;
-            }
-
-            int convertIndex = numberToDelete - 1;
-
-            fullNames = RemoveElement(convertIndex, fullNames);
-            positions = RemoveElement(convertIndex, positions);
-        }
-
-        private static string[] RemoveElement(int removeIndex, string[] array)
-        {
-            string[] tempArray = new string[array.Length - 1];
-
-            for (int i = 0; i < removeIndex; i++)
-            {
-                tempArray[i] = array[i];
-            }
-
-            for (int i = removeIndex + 1; i < array.Length; i++)
-            {
-                tempArray[i - 1] = array[i];
-            }
-
-            return tempArray;
-        }
-
-        private static bool ValidateIndexRange(int index, int maxIndexLimit)
-        {
-            int minIndexLimit = 0;
-
-            return index >= minIndexLimit && index < maxIndexLimit;
-        }
-
-        static void SearchByLastName(string[] fullName, string[] Position)
-        {
-            Console.Write("Введите фамилию для поиска: ");
-            string surnameToFind = Console.ReadLine();
-
-            string[] tempArray;
-            bool isDossierExist = false;
-
-            for (int i = 0; i < fullName.Length; i++)
-            {
-                tempArray = fullName[i].Split();
-
-                if (surnameToFind.ToLower() == tempArray[0].ToLower())
-                {
-                    Console.WriteLine($"ФИО сотрудник: {fullName[i]}. Должность: {Position[i]}");
-                    isDossierExist = true;
-                }
-            }
-
-            if (isDossierExist == false)
-            {
-                ChangeColor(ConsoleColor.Red,"С такой фамилией досье не найдено...");
-            }
-        }
-
-        private static void ChangeColor(ConsoleColor color, string text)
-        {
-            Console.ForegroundColor = color;
-            Console.WriteLine(text);
-            Console.ResetColor();
+            return isOpenGame;
         }
     }
 }
