1db354d8b09244a8509b6b9b30509458a8dfd581
yturf <150423924+yturf@users.noreply.github.com>
2025-08-10 20:30:14 +0500

    Add files via upload

---
+++ Толковый словарь.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Толковый_словарь
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> ExplanatoryDictionary = new Dictionary<string, string>();
+
+            ExplanatoryDictionary.Add("Друг", "человек, близкий по духу");
+            ExplanatoryDictionary.Add("Забота", "внимание, поддержка, оказание помощи");
+            ExplanatoryDictionary.Add("Задабривание", "умасливание лестью, услугами, подарками");
+
+            const string CommandExit = "Выйти";
+
+            bool isOpen = true;
+
+            Console.WriteLine("Все слова в словаре.");
+            Console.WriteLine("--------------------");
+
+            foreach (var key in ExplanatoryDictionary.Keys)
+            {
+                Console.WriteLine(key);
+            }
+
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Введите слово, значение которого хотите узнать:");
+
+            Console.WriteLine("Для выхода из словаря введите - " + CommandExit);
+
+            while (isOpen)
+            {
+                string userInput = Console.ReadLine();
+
+                if (userInput == GetWord(ExplanatoryDictionary, userInput))
+                if (userInput.ToLower() == CommandExit.ToLower())
+                    isOpen = false;
+                else
+                    Console.WriteLine("Такого слова нет...");
+            }
+
+        }
+        static string GetWord(Dictionary<string, string> dictionary, string needWord)
+        {
+            foreach (var key in dictionary)
+            {
+                if (needWord == key.Key)
+                {
+                    Console.WriteLine($"Слово - {key.Key}. Его значение - {key.Value}.");
+                }
+            }
+
+            return needWord;
+        }
+    }
+}
