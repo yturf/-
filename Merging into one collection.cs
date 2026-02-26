9128615217b5703dab0978c3bf2b6d542b1c669f
yturf <150423924+yturf@users.noreply.github.com>
2026-02-26 21:48:38 +0500

    Add files via upload

--- Merging into one collection.cs
+++ Merging into one collection.cs
@@ -7,20 +7,20 @@ namespace Merging_into_one_collection
     {
         static void Main(string[] args)
         {
-            string[] array1 = new string[] { "один", "три", "два" };
+            string[] array1 = new string[] { "один", "один", "три", "два" };
             string[] array2 = new string[] { "три", "три", "четыре", "пять" };
 
             List<string> mergeList = new List<string>();
-            mergeList.AddRange(array1);
 
-            MergeList(mergeList, array2);
+            AddUniqueValues(mergeList, array1);
+            AddUniqueValues(mergeList, array2);
 
             ShowMergedList(mergeList);
 
             Console.ReadKey();
         }
 
-        public static void MergeList(List<string> mergeList, string[] array)
+        public static void AddUniqueValues(List<string> mergeList, string[] array)
         {
             foreach (var item in array)
             {
