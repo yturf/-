ac545c1108769802c9c5d2d1d1e182491d03c187
yturf <150423924+yturf@users.noreply.github.com>
2026-04-30 20:16:54 +0500

    Add files via upload

---
+++ WorkingWithClasses.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WorkingWithClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player("Bob", 20, 300);
+
+            player.ShowInfo();
+        }
+    }
+
+    class Player
+    {
+        public string Name;
+        public int Age;
+        public int Health;
+
+        public Player(string name, int age, int health)
+        {
+            Name = name;
+            Age = age;
+            Health = health;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Имя - {Name}\nВозраст - {Age}\nЗдоровье - {Health}");
+        }
+    }
+}
